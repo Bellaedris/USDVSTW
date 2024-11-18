ed7d738538aef1c7ba6aa07ef753608bfde70e43
Arthur Pradier <arthur.pradier81@gmail.com>
2024-11-18 14:00:26 +0100

    feature(Weapons): Projectiles orientation
    Projectiles aren't attached to the player anymore and will correctly spawn

--- Assets/Scripts/Weapons/Linear.cs
+++ Assets/Scripts/Weapons/Linear.cs
@@ -9,7 +9,7 @@ namespace usd.Weapons
             float spawnOffset = 1f / (numberOfProjectile + 1f);
             for (int i = 0; i < numberOfProjectile; i++)
             {
-                Instantiate(projectilePrefab, transform.position + new Vector3(0f, -.5f + spawnOffset * (i + 1), 0f), Quaternion.Euler(90f, 0f, 0f));
+                Instantiate(projectilePrefab, transform.position + new Vector3(0f, -.5f + spawnOffset * (i + 1), 0f), transform.rotation );
             }
         }
     }
