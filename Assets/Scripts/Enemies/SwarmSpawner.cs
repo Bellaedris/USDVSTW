7819fea40e5193499d43f9a1ebb01c54ad2572b2
NajibXY <naj.elkhadir@gmail.com>
2024-12-02 04:20:52 +0100

    [FEAT - ENEMIES] Added Swarm enemies with a randomized direction every x sec based on player position too

---
+++ Assets/Scripts/Enemies/SwarmSpawner.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using usd.Enemies.Projectiles;
+
+namespace usd.Enemies
+{
+    public class SwarmSpawner : BasicEnemy
+    {
+        public int numberOfUnits;
+        public GameObject unitPrefab;
+        
+        private List<GameObject> swarmUnits;
+        private Vector3 moveDirection;
+        
+        public Vector3 GetMoveDirection()
+        {
+            return moveDirection;
+        }
+
+        public void CalculateRandomDirection()
+        {
+            // Calculate direction towards player position
+            Vector3 playerDirection = (player.transform.position - transform.position).normalized;
+
+            // Determine possible directions based on player position
+            List<Vector3> directions = new List<Vector3>();
+
+            if (playerDirection.x < 0)
+            {
+                directions.Add(Vector3.left);
+                directions.Add(new Vector3(-1, 1, 0).normalized);
+                directions.Add(new Vector3(-1, -1, 0).normalized);
+            }
+            else if (playerDirection.x > 0)
+            {
+                directions.Add(Vector3.right);
+                directions.Add(new Vector3(1, 1, 0).normalized);
+                directions.Add(new Vector3(1, -1, 0).normalized);
+            }
+
+            if (playerDirection.y < 0)
+            {
+                directions.Add(Vector3.down);
+            }
+            else if (playerDirection.y > 0)
+            {
+                directions.Add(Vector3.up);
+            }
+
+            moveDirection = directions[Random.Range(0, directions.Count)];
+
+            // Give direction to swarm units
+            foreach (var swarmUnit in swarmUnits)
+            {
+                swarmUnit.GetComponent<SwarmUnit>().SetMoveDrection(moveDirection);
+            }
+        }
+        
+        public void RemoveUnit(SwarmUnit swarmUnit)
+        {
+            swarmUnits.Remove(swarmUnit.gameObject);
+            numberOfUnits--;
+        }
+        
+        void Start()
+        {
+            // Offset position by a fixed value depending on number of units, so that it spawns around the spawner
+            player = GameObject.Find("player");
+            int unitsPerSide = Mathf.CeilToInt(Mathf.Sqrt(numberOfUnits));
+            float spacing = 0.5f;
+            swarmUnits = new List<GameObject>();
+            
+            for (int i = 0; i < numberOfUnits; i++)
+            {
+                int row = i / unitsPerSide;
+                int col = i % unitsPerSide;
+                float xOffset = (col - row) * spacing;
+                float yOffset = (col + row) * spacing;
+                Vector3 offset = new Vector3(xOffset, yOffset, 0);
+                GameObject swarmUnit = Instantiate(unitPrefab, transform.position + offset, Quaternion.identity);
+                swarmUnits.Add(swarmUnit);
+                swarmUnit.GetComponent<SwarmUnit>().InitializeValues(this);
+            }
+            
+            CalculateRandomDirection();
+            // Call CalculateRandomDirection every 6 seconds
+            InvokeRepeating("CalculateRandomDirection", 0.0f, Random.Range(4.0f, 8.0f));
+        }
+        
+        void Update()
+        {
+            transform.position = transform.position + moveDirection * movementSpeed * Time.deltaTime;
+            
+            if(!limits.Contains(transform.position))
+                CalculateRandomDirection();
+            
+            // Death of spawner if all units dead
+            if (numberOfUnits <= 0)
+            {
+                SpawnerDieAndDrop();
+            }
+        }
+
+        void SpawnerDieAndDrop()
+        {
+            // Drop loot before death
+            var hasDropped = false;
+            for (int i = 0; i < dropPrefab.Count; i++)
+            {
+                if (Random.Range(0f, 1f) < dropRate[i] && !hasDropped)
+                {
+                    Instantiate(dropPrefab[i], transform.position, Quaternion.identity);
+                    hasDropped = true;
+                }
+            }
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Enemies/SwarmUnit.cs
@@ -0,0 +1,132 @@
+﻿using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using usd.Enemies.Projectiles;
+
+namespace usd.Enemies
+{
+    public class SwarmUnit : BasicEnemy
+    {
+        private SwarmSpawner spawnerReference;
+        private Vector3 moveDirection;
+        
+        public void SetMoveDrection(Vector3 direction)
+        {
+            moveDirection = direction;
+        }
+        
+        void Start()
+        {
+            timeLastShot = 0.0f;
+            player = GameObject.Find("player");
+            playerPosition = player.transform.position;
+        }
+        
+        public void InitializeValues(SwarmSpawner spawner)
+        {
+            // Set the player reference to the player reference of the spawner
+            // Initialize all public fields
+            health = spawner.health;
+            limits = spawner.limits;
+            movementSpeed = spawner.movementSpeed;
+            projectileSpeed = spawner.projectileSpeed;
+            projectileDamage = spawner.projectileDamage;
+            fireRate = spawner.fireRate;
+            fireLineSize = spawner.fireLineSize;
+            fireProjectilesCount = spawner.fireProjectilesCount;
+            projectilePrefab = spawner.projectilePrefab;
+            scoreValue = spawner.scoreValue;
+            dropPrefab = new List<GameObject>(spawner.dropPrefab);
+            dropRate = new List<float>(spawner.dropRate);
+            spawnerReference = spawner;
+        }
+        void Update()
+        {
+            timeLastShot += Time.deltaTime;
+            playerPosition = player.transform.position;
+            
+            // Move towards player
+            Move();
+            
+            // // Shoot if possible
+            if (limits.Contains(transform.position) && CanShoot())
+            {
+                Shoot();
+            }
+            else if (!limits.Contains(transform.position))
+            {
+                Destroy(gameObject);
+                spawnerReference.RemoveUnit(this);
+            }
+        }
+        
+        // Override take damage and die methods
+        
+        public new void TakeDamage(int damageTaken)
+        {
+            health -= damageTaken;
+            if (health <= 0)
+            {
+                Destroy(gameObject);
+                spawnerReference.RemoveUnit(this);
+            }
+        }
+        
+        // Specific Methods
+        public override bool CanShoot()
+        {
+            if (timeLastShot >= 1.0f/fireRate)
+            {
+                timeLastShot = 0.0f;
+                return true;
+            }
+            
+            return false;
+        }
+        
+        public override void Shoot()
+        {   
+            //Todo maybe remove fireLine for basic nmy
+            if (fireLineSize > 0 && fireProjectilesCount > 1)
+            {
+                Vector3 fireDirection = playerPosition - transform.position;
+                fireDirection.z = 0;
+                fireDirection = fireDirection.normalized;
+                
+                float spacing = fireLineSize / (fireProjectilesCount - 1);
+
+                // Calculate perpendicular direction for spreading projectiles along the fire line
+                Vector3 perpendicular = new Vector3(-fireDirection.y, fireDirection.x, 0);
+
+                // Start position relative to the center of the line
+                Vector3 startPosition = transform.position - perpendicular * (fireLineSize / 2f);
+
+                for (int i = 0; i < fireProjectilesCount; i++)
+                {
+                    // Calculate the position for each projectile
+                    Vector3 spawnPosition = startPosition + perpendicular * (spacing * i);
+
+                    // Instantiate the projectile at the calculated world position
+                    GameObject projectile = Instantiate(projectilePrefab, spawnPosition, Quaternion.identity);
+                    projectile.GetComponent<NmyLinearProjectile>().speed = projectileSpeed;
+                    projectile.GetComponent<NmyLinearProjectile>().damage = projectileDamage;
+                    projectile.GetComponent<NmyLinearProjectile>().target = playerPosition;
+                }
+            }
+            else
+            { 
+                GameObject projectile = Instantiate(projectilePrefab, transform.position, Quaternion.identity);
+                // Todo change sript to linear projectile enemy
+                projectile.GetComponent<NmyLinearProjectile>().speed = projectileSpeed;
+                projectile.GetComponent<NmyLinearProjectile>().damage = projectileDamage;
+                projectile.GetComponent<NmyLinearProjectile>().target = playerPosition;
+            }
+        }
+
+        public override void Move()
+        {
+            // Move according to swarm direction
+            transform.position = transform.position + moveDirection * movementSpeed * Time.deltaTime;         
+        }
+    }
+}
